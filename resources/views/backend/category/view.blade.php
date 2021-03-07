a56ddb70721cd3289d7562172452957437df8ec2
MaheKarim <mahekarim@gmail.com>
2021-03-07 18:21:29 +0600

    Advanced Admin Distract

---
+++ resources/views/backend/category/view.blade.php
@@ -0,0 +1,13 @@
+@extends('home')
+
+@section('content')
+<div class="container-fluid">
+    <h1 class="mt-4">Category</h1>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Category</li>
+        </ol>
+      </nav>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/category', 'CategoryController@index')->name('admin.category');
