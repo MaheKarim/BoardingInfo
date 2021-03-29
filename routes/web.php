72d049b14377e84070d600447514f142a3352202
MaheKarim <mahekarim@gmail.com>
2021-03-30 04:09:55 +0600

    Sidebar Improve

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/user/dashboard', 'UserController@index')->name('user.home');
 Route::get('/admin/dashboard', 'AdminController@index')->name('admin.home');
